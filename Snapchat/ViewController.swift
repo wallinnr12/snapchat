91da1cc695f634801134bcffd6d63e9ed6ffcf7f
Johan Wallin <wallinnr12@gmail.com>
2019-05-09 17:44:37 +0200

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Johan on 2019-05-09.
+//  Copyright © 2019 Johan W. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
